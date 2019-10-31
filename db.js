c6cab13132e8af36899d47bd055318c59c205ea0
Jeya Raniga <jayafj@gmail.com>
2019-11-01 11:54:43 +1300

    minor file naming and ran migrations

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
-const environment = process.env.NODE_ENV || "development";
-const config = require("./knexfile")[environment];
-const connection = require("knex")(config);
+const environment = process.env.NODE_ENV || 'development';
+const config = require('./knexfile')[environment]
+const connection = require('knex')(config)
 
 module.exports = {
   getUser: getUser,
   getUsers: getUsers
-};
-
-function getUsers(db = connection) {
-  return db("users").select();
 }
 
-function getUser(id, db = connection) {
-  return db("users")
-    .where("id", id)
-    .first();
+function getUsers (db = connection) {
+  return db('players').select()
+}
+
+function getUser (id, db = connection) {
+  return db('players')
+    .where('id', id)
+    .first()
 }
--- routes/users.js
+++ routes/users.js
@@ -1,42 +1,44 @@
-const express = require("express");
+const express = require('express')
 
-const db = require("../db");
+const db = require('../db')
 
-const router = express.Router();
+const router = express.Router()
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   db.getUsers()
     .then(users => {
-      res.render("index", { users: users });
+      res.render('index', { users: users })
     })
     .catch(err => {
-      res.status(500).send("DATABASE ERROR: " + err.message);
-    });
-});
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
 
-router.get("/card/:playerId", (req, res) => {
-  const cardId = getRandomInt(1, 5);
-  const playerId = req.params.playerId;
-  db.getUser(playerId).then(player => {
-    db.getCard(cardId).then(card => {
-      const playerName = player.playername;
-      const cardWithPlayerName = {
-        player: playerName,
-        card: card
-      };
-      res.render("card", { card: cardWithPlayerName });
-    });
-  });
-});
+router.get('/card/:playerId', (req, res) => {
+  const cardId = getRandomInt(1, 5)
+  const playerId = req.params.playerId
+  db.getUser(playerId)
+    .then(player => {
+      db.getCard(cardId)
+        .then(card => {
+          const playerName = player.playername
+          const cardWithPlayerName = {
+            player: playerName,
+            card: card
+          }
+          res.render('card', { card: cardWithPlayerName })
+        })
+    })
+})
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+function getRandomInt (min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min)) + min
 }
 
-router.get("/start", (req, res) => {
-  res.render("start");
-});
+router.get('/start', (req, res) => {
+  res.render('start')
+})
 
-module.exports = router;
+module.exports = router

430574faee034d150a12d275042585d5a1284170
SunnyNiu <sunnyyingli@gmail.com>
2019-11-02 12:34:58 +1300

    remove hardcode and add clear users after 3 round playiing

--- db.js
+++ db.js
@@ -1,33 +1,43 @@
-const environment = process.env.NODE_ENV || 'development'
-const config = require('./knexfile')[environment]
-const connection = require('knex')(config)
+const environment = process.env.NODE_ENV || "development";
+const config = require("./knexfile")[environment];
+const connection = require("knex")(config);
 
 module.exports = {
   getUser: getUser,
   getUsers: getUsers,
   getCard: getCard,
-  insertPlayer: insertPlayer
+  insertPlayer: insertPlayer,
+  clearPlayers: clearPlayers
+};
+
+function getUsers(db = connection) {
+  return db("players").select();
 }
 
-function getUsers (db = connection) {
-  return db('players').select()
+function getUser(id, db = connection) {
+  console.log(id);
+  return db("players")
+    .where("players.id", Number(id))
+    .first();
 }
 
-function getUser (id, db = connection) {
-  console.log(id)
-  return db('players')
-    .where('players.id', Number(id))
-    .first()
+function getCard(cardId, db = connection) {
+  return db("cards")
+    .where("id", cardId)
+    .first();
 }
 
-
-function getCard (cardId, db = connection) {
-  return db('cards')
-    .where('id', cardId)
-    .first()
+function insertPlayer(playername, db = connection) {
+  return db("players").insert({ playername: playername });
 }
 
-function insertPlayer (playername, db = connection) {
-  return db('players').insert({ playername: playername })
+function clearPlayers(db = connection) {
+  return db("players")
+    .del()
+    .then(() =>
+      db("SQLITE_SEQUENCE")
+        .update({ SEQ: 0 })
+        .where("NAME", "players")
+    )
+    .catch(error => console.log(error));
 }
-
--- routes/users.js
+++ routes/users.js
@@ -1,70 +1,79 @@
-const express = require('express')
+const express = require("express");
 
-const db = require('../db')
+const db = require("../db");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   db.getUsers()
     .then(playername => {
-      res.render('index', { players: playername })
+      res.render("index", { players: playername });
     })
     .catch(err => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
-})
+      res.status(500).send("DATABASE ERROR: " + err.message);
+    });
+});
 
-router.get('/addPlayer', (req, res) => {
-  res.render('addPlayer', {})
-})
+router.get("/addPlayer", (req, res) => {
+  res.render("addPlayer", {});
+});
 
-router.post('/addPlayer', (req, res) => {
-  db.insertPlayer(req.body.name).then(res.redirect('/'))
-})
+router.post("/addPlayer", (req, res) => {
+  db.insertPlayer(req.body.name).then(res.redirect("/"));
+});
 
-const totalCount = 16
-let count = 1
-router.get('/card/:playerId', (req, res) => {
-  if (count > totalCount) {
-    res.redirect('/')
-  } else {
-    const cardId = getRandomInt(1, 15)
+const round = 3;
+let count = 1;
+router.get("/card/:playerId", (req, res) => {
+  let usersNumber = 0;
+  db.getUsers()
+    .then(users => (usersNumber = users.length))
+    .then(usersNumber => {
+      const totalCount = round * usersNumber;
+      if (count > totalCount) {
+        db.clearPlayers().then(() => {
+          count = 1;
+          res.redirect("/");
+        });
+      } else {
+        const cardId = getRandomInt(1, 15);
 
-    const playerId = Number(req.params.playerId)
-    const nextPlayerId = playerId + 1
+        const playerId = Number(req.params.playerId);
+        const nextPlayerId = playerId + 1;
 
-    if (playerId < 6) {
-      db.getUser(playerId).then(player => {
-        const playerName = player.playername
-        db.getCard(cardId).then(card => {
-          const cardWithPlayerName = {
-            playerId: nextPlayerId,
-            player: playerName,
-            card: card
-          }
-          count++
-          res.render('card', { card: cardWithPlayerName })
-        })
-      })
-    } else {
-      res.redirect('/card/1')
-    }
-  }
-})
+        if (playerId <= usersNumber) {
+          db.getUser(playerId).then(player => {
+            const playerName = player.playername;
+            db.getCard(cardId).then(card => {
+              const cardWithPlayerName = {
+                playerId: nextPlayerId,
+                player: playerName,
+                card: card
+              };
+              count++;
+              res.render("card", { card: cardWithPlayerName });
+            });
+          });
+        } else {
+          res.redirect("/card/1");
+        }
+      }
+    });
+});
 
-function getRandomInt (min, max) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min)) + min
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
-router.get('/start', (req, res) => {
-  res.render('start')
-})
+router.get("/start", (req, res) => {
+  res.render("start");
+});
 
-function consoleLog (user) {
-  console.log(user)
-  return Promise.resolve(user)
+function consoleLog(user) {
+  console.log(user);
+  return Promise.resolve(user);
 }
 
-module.exports = router
+module.exports = router;
